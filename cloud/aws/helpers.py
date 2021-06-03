ea2424cd183eff325c66dae7a7e9f9a046c56c14
jozo <hi@jozo.io>
2021-06-03 14:00:14 +0200

    #1 - Use boto3 instead of AWS CLI

--- cloud/aws/helpers.py
+++ cloud/aws/helpers.py
@@ -1,43 +1,55 @@
-import json
 import logging
-import os
-import sys
 
+import boto3
+from botocore.exceptions import BotoCoreError
 from unfurl.vendor.toscaparser.elements.scalarunit import ScalarUnit_Size
-
-if os.name == 'posix' and sys.version_info[0] < 3:
-    import subprocess32 as subprocess
-else:
-    import subprocess
-
 
 log = logging.getLogger(__file__)
 
 
 def choose_machine_type(ctx):
     """Choose machine type based on memory and cpu"""
+    num_cpus, mem_size = attributes_from_host(ctx)
+    types = machine_types(num_cpus, mem_size)
+
+    if types:
+        return types[0]
+    raise ValueError(
+        "Can't find satisfactory machine type ({} cpus, {} mem).".format(
+            num_cpus, mem_size
+        )
+    )
+
+
+def attributes_from_host(ctx):
     host = ctx.currentResource.capabilities.get("host")
     if not host:
         raise ValueError("Can't choose machine type - host info not provided")
     if "num_cpus" not in host.properties or "mem_size" not in host.properties:
-        raise ValueError("Can't choose machine type - num_cpus and mem_size must be provided")
+        raise ValueError(
+            "Can't choose machine type - num_cpus and mem_size must be provided"
+        )
 
     num_cpus = host.properties["num_cpus"]
     mem_size = host.properties["mem_size"]
     mem_size = ScalarUnit_Size(mem_size).get_num_from_scalar_unit("MB")
-    aws_command = "aws ec2 describe-instance-types --query InstanceTypes[].InstanceType " \
-                  "--filters Name=memory-info.size-in-mib,Values={} Name=vcpu-info.default-vcpus,Values={} " \
-                  "--max-items 1".format(mem_size, num_cpus)
+    return num_cpus, mem_size
+
+
+def machine_types(num_cpus, mem_size):
+    client = boto3.client("ec2")
+    filters = [
+        {"Name": "vcpu-info.default-vcpus", "Values": [str(num_cpus)]},
+        {"Name": "memory-info.size-in-mib", "Values": [str(mem_size)]},
+    ]
     try:
-        process = subprocess.run(aws_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError as e:
-        log.error("AWS CLI: %s", e.stderr)
+        response = client.describe_instance_types(Filters=filters)
+    except BotoCoreError as e:
+        log.error("AWS: %s", e)
         raise ValueError(
-            "Can't find machine type ({} cpus, {} mem). Can't communicate with AWS."
-            .format(num_cpus, mem_size)
+            "Can't find machine type ({} cpus, {} mem). Can't communicate with AWS.".format(
+                num_cpus, mem_size
+            )
         )
 
-    data = json.loads(process.stdout)
-    if data:
-        return data[0]
-    raise ValueError("Can't find satisfactory machine type ({} cpus, {} mem).".format(num_cpus, mem_size))
+    return [it["InstanceType"] for it in response["InstanceTypes"]]
