af5cdf7c41ac0b7db29d3bc978fbc8fc5de698c2
jozo <hi@jozo.io>
2021-07-07 11:40:36 +0200

    #1 - Add metadata node which saves machine types

--- cloud/aws/helpers.py
+++ cloud/aws/helpers.py
@@ -3,14 +3,15 @@ import logging
 import boto3
 from botocore.exceptions import BotoCoreError
 from toscaparser.elements.scalarunit import ScalarUnit_Size
+from unfurl.configurator import Configurator
 
 log = logging.getLogger(__file__)
 
 
-def choose_machine_type(ctx):
+def choose_machine_type(ctx, args):
     """Choose machine type based on memory and cpu"""
     num_cpus, mem_size = attributes_from_host(ctx)
-    types = all_machine_types()
+    types = args["machine_types"]
     types = filter(lambda x: x["mem"] >= mem_size and x["cpu"] >= num_cpus, types)
     types = sorted(types, key=lambda x: (x["cpu"], x["mem"]))
 
@@ -30,38 +31,44 @@ def choose_machine_type(ctx):
 
 
 def attributes_from_host(ctx):
-    host = ctx.currentResource.capabilities.get("host")
+    host = None
+    for capability in ctx.currentResource.capabilities:
+        if capability.name == "host":
+            host = capability
+            break
     if not host:
         raise ValueError("Can't choose machine type - host info not provided")
-    if "num_cpus" not in host.properties or "mem_size" not in host.properties:
+    if "num_cpus" not in host.attributes or "mem_size" not in host.attributes:
         raise ValueError(
             "Can't choose machine type - num_cpus and mem_size must be provided"
         )
 
-    num_cpus = host.properties["num_cpus"]
-    mem_size = host.properties["mem_size"]
+    num_cpus = host.attributes["num_cpus"]
+    mem_size = host.attributes["mem_size"]
     mem_size = ScalarUnit_Size(mem_size).get_num_from_scalar_unit("MiB")
     return num_cpus, mem_size
 
 
-def all_machine_types():
-    client = boto3.client("ec2")
-    kwargs = {}
-    try:
-        while True:
-            results = client.describe_instance_types(**kwargs)
-            if "NextToken" not in results:
-                break
-            kwargs["NextToken"] = results["NextToken"]
-            yield from (
-                {
-                    "name": it["InstanceType"],
-                    "mem": it["MemoryInfo"]["SizeInMiB"],
-                    "cpu": it["VCpuInfo"]["DefaultVCpus"],
-                }
-                for it in results["InstanceTypes"]
-            )
+class MetadataConfigurator(Configurator):
+    def run(self, task):
+        task.logger.info("Fetching machine types")
+        task.target.attributes["machine_types"] = list(self.all_machine_types())
+        yield task.done(True)
 
-    except BotoCoreError as e:
-        log.error("AWS: %s", e)
-        raise ValueError("Can't find machine types. Can't communicate with AWS.")
+    @staticmethod
+    def all_machine_types():
+        client = boto3.client("ec2")
+        try:
+            paginator = client.get_paginator("describe_instance_types")
+            for page in paginator.paginate():
+                yield from (
+                    {
+                        "name": it["InstanceType"],
+                        "mem": it["MemoryInfo"]["SizeInMiB"],
+                        "cpu": it["VCpuInfo"]["DefaultVCpus"],
+                    }
+                    for it in page["InstanceTypes"]
+                )
+        except BotoCoreError as e:
+            log.error("AWS: %s", e)
+            raise ValueError("Can't find machine types. Can't communicate with AWS.")
--- cloud/gcp/helpers.py
+++ cloud/gcp/helpers.py
@@ -1,16 +1,18 @@
 import logging
+import os
 
 from google.api_core.exceptions import GoogleAPIError
 from google.cloud.compute_v1 import ListMachineTypesRequest, MachineTypesClient
 from toscaparser.elements.scalarunit import ScalarUnit_Size
+from unfurl.configurator import Configurator
 
 log = logging.getLogger(__file__)
 
 
-def choose_machine_type(ctx):
+def choose_machine_type(ctx, args=None):
     """Choose machine type based on memory and cpu"""
     num_cpus, mem_size = attributes_from_host(ctx)
-    types = all_machine_types()
+    types = args["machine_types"]
     types = filter(lambda x: x["mem"] >= mem_size and x["cpu"] >= num_cpus, types)
     types = sorted(types, key=lambda x: (x["cpu"], x["mem"]))
 
@@ -30,37 +32,55 @@ def choose_machine_type(ctx):
 
 
 def attributes_from_host(ctx):
-    host = ctx.currentResource.capabilities.get("host")
+    host = None
+    for capability in ctx.currentResource.capabilities:
+        if capability.name == "host":
+            host = capability
+            break
     if not host:
         raise ValueError("Can't choose machine type - host info not provided")
-    if "num_cpus" not in host.properties or "mem_size" not in host.properties:
+
+    if "num_cpus" not in host.attributes or "mem_size" not in host.attributes:
         raise ValueError(
             "Can't choose machine type - num_cpus and mem_size must be provided"
         )
 
-    num_cpus = host.properties["num_cpus"]
-    mem_size = host.properties["mem_size"]
+    num_cpus = host.attributes["num_cpus"]
+    mem_size = host.attributes["mem_size"]
     mem_size = ScalarUnit_Size(mem_size).get_num_from_scalar_unit("MB")
     return num_cpus, mem_size
 
 
-def all_machine_types():
-    request = ListMachineTypesRequest()
-    request.project = "unfurl-test"         # Change to your GCP Compute project
-    request.zone = "us-central1-a"
+class MetadataConfigurator(Configurator):
+    def run(self, task):
+        task.logger.info("Fetching machine types")
+        task.target.attributes["machine_types"] = list(self.all_machine_types())
+        yield task.done(True)
 
-    try:
-        client = MachineTypesClient()
-        response = client.list(request)
-    except GoogleAPIError as e:
-        log.error("GCP: %s", e)
-        raise ValueError("Can't find machine types. Can't communicate with GCP.")
+    @staticmethod
+    def all_machine_types():
+        request = ListMachineTypesRequest()
+        project = os.getenv("GOOGLE_PROJECT")
+        zone = os.getenv("GOOGLE_ZONE")
+        if not project or not zone:
+            raise ValueError(
+                "Can't choose machine type - GOOGLE_ZONE or GOOGLE_PROJECT not defined"
+            )
+        request.project = project
+        request.zone = zone
 
-    yield from (
-        {
-            "name": item.name,
-            "mem": item.memory_mb,
-            "cpu": item.guest_cpus,
-        }
-        for item in response.items
-    )
+        try:
+            client = MachineTypesClient()
+            response = client.list(request)
+        except GoogleAPIError as e:
+            log.error("GCP: %s", e)
+            raise ValueError("Can't find machine types. Can't communicate with GCP.")
+
+        yield from (
+            {
+                "name": item.name,
+                "mem": item.memory_mb,
+                "cpu": item.guest_cpus,
+            }
+            for item in response.items
+        )
